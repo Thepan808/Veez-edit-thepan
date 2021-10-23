66865f7a6e29201a719d535ac8eae3175755ea2b
Thepan808 <89053669+Thepan808@users.noreply.github.com>
2021-10-22 22:35:00 -0300

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -671,10 +671,7 @@ async def play(_, message: Message):
                     [InlineKeyboardButton(text="🗑 Fechar", callback_data="cls")],
                 ]
             )
-            await message.reply_photo(
-                photo=f"{THUMB_IMG}",
-                caption=toxxt,
-                reply_markup=keyboard,
+            photo=f"{THUMB_IMG}", caption=toxxt, reply_markup=keyboard
             )
 
             await lel.delete()
