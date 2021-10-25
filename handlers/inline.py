4f945cf8a2ff77e5a05316ae1e1c84c13671c722
ঔৣ͜͡➳ 亇ቾ❍ ✟ア入れ刃入✟ <89053669+Thepan808@users.noreply.github.com>
2021-10-25 18:03:11 -0300

    Update inline.py

--- handlers/inline.py
+++ handlers/inline.py
@@ -16,7 +16,7 @@ async def inline(client: Client, query: InlineQuery):
         await client.answer_inline_query(
             query.id,
             results=answers,
-            switch_pm_text="♦ Pesquise sua vossa música, ou vídeo meo parceiru ♦...",
+            switch_pm_text="♦ Pesquise sua vossa música / vídeo ♦...",
             switch_pm_parameter="help",
             cache_time=0,
         )
