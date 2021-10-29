496d38c4589e0b151bbec331b251ec6845c1529a
ঔৣ͜͡➳ 亇ቾ❍ ✟ア入れ刃入✟ <89053669+Thepan808@users.noreply.github.com>
2021-10-29 14:26:32 -0300

    Update rmtrash.py

--- handlers/rmtrash.py
+++ handlers/rmtrash.py
@@ -9,8 +9,9 @@ from config import BOT_USERNAME
 from helpers.decorators import errors, sudo_users_only
 from helpers.filters import command
 
+
 downloads = os.path.realpath("downloads")
-raw = os.path.realpath("raw_files")
+raw = os.path.realpath("raw_files") # the code is not created for removing raw_files but if you want to create it, use this
 
 
 @Client.on_message(command(["rmd", "clean", f"rmd@{BOT_USERNAME}", f"clean@{BOT_USERNAME}"]) & ~filters.edited)
@@ -23,4 +24,18 @@ async def clear_downloads(_, message: Message):
             os.remove(os.path.join(downloads, file))
         await message.reply_text("✅ **Removi todos os download files**")
     else:
-        await message.reply_text("❌ **Nem tem files, como vai remover desgraça?**")
+        await message.reply_text("❌ **Não tem downloads files**")
+
+
+@Client.on_message(command(["clear", f"clear@{BOT_USERNAME}"]) & ~filters.edited)
+@errors
+@sudo_users_only
+async def clear_jpg_image(_, message: Message):
+    pth = os.path.realpath(".")
+    ls_dir = os.listdir(pth)
+    if ls_dir:
+        for dta in os.listdir(pth):
+            os.system("rm -rf *.jpg")
+        await message.reply_text("✅ **Apagado com sucesso**")
+    else:
+        await message.reply_text("✅ **já está limpo**")
